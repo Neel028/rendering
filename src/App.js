8fd6470d4c1c23ad8b0ffa44045e32f9d076ce22
Neel028 <neelkoshiya028@gmail.com>
2023-03-30 23:04:36 +0530

    finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
 
 function App() {
+  let[state,setState]=useState(1);
+  let[state1,setState1]=useState(1);
+  
+  function i4(){
+    setState1(state1=state1-1)
+  }
+  function i3(){
+    setState(state=state-1)
+  }
+  function i2(){
+    setState1(++state1)
+  }
+
+  function i1(){
+    setState(++state)
+  }
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={i1}>+1 in frist number</button>
+      <button onClick={i2}>+1 in second number</button>
+      <button onClick={i3}>-1 in frist number</button>
+      <button onClick={i4}>-1 in second number</button>
+
+      <h1>{state}*{state1}={state*state1}</h1>
     </div>
+    
   );
+  
 }
 
 export default App;
